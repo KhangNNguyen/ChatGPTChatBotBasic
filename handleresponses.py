6658396a6c1328930a540f9f09b2db462b9e04f5
KhangNNguyen <128857346+KhangNNguyen@users.noreply.github.com>
2024-08-22 11:30:01 -0700

    edited to handle 2000+ character responses

--- handleresponses.py
+++ handleresponses.py
@@ -4,4 +4,7 @@ def handle_response(message) -> str:
     context_file = "prompt.txt"
     context_message = aitest.load_context_from_file(context_file)
     response = aitest.chat_with_gpt(message, context_message)
-    return response
+    if len(response) > 2000:
+        return ("The response is greater than 2000 characters (discord limit) please revise your prompt to limit the response to under 2000 characters. :nerd: ")
+    else:
+        return response
