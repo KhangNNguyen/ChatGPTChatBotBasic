27b9ec05e0ef215eb6b847ea8b8c54ec29bb08af
Khang Nguyen <128857346+KhangNNguyen@users.noreply.github.com>
2024-08-22 18:35:19 -0700

    Add files via upload

--- handleresponses.py
+++ handleresponses.py
@@ -5,6 +5,9 @@ def handle_response(message) -> str:
     context_message = aitest.load_context_from_file(context_file)
     response = aitest.chat_with_gpt(message, context_message)
     if len(response) > 2000:
-        return ("The response is greater than 2000 characters (discord limit) please revise your prompt to limit the response to under 2000 characters. :nerd: ")
+        out = [(response[i:i+1900]) for i in range (0,len(response), 1900)]
+        return out
+        #return ("The response is greater than 2000 characters (discord limit) please revise your prompt to limit the response to under 2000 characters. :nerd: ")
     else:
-        return response
+        out = [response]
+        return out
